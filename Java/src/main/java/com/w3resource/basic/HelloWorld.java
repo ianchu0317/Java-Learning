087a8199bc95691dd6020020fb51feaea9ef2ea4
Ian Chen <71509578+ianchu0317@users.noreply.github.com>
2022-06-09 00:19:57 -0300

    First Commit

---
+++ Java/src/main/java/com/w3resource/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.w3resource.basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
